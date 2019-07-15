e403892a7fb973ef1616b05b6b670d43a9abee6d
dxshim90 <dxshim90@gmail.com>
2019-07-15 16:56:07 +0100

    scroll added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import "./App.css";
+import Scroll from "./Scroll";
 
 class App extends React.Component {
   state = {
@@ -40,7 +41,9 @@ class App extends React.Component {
         <div className="tc">
           <h1 className="f1">Address Book!</h1>
           <SearchBox searchChange={this.onSearchChange} />
-          <CardList robots={filter} />
+          <Scroll>
+            <CardList robots={filter} />
+          </Scroll>
         </div>
       );
     }
